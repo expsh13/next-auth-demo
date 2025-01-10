e6a453f5fadb2a63620e6beec5e09dc9d3fa9463
Satoki Hata <satokihata@gmail.com>
2025-01-10 17:15:06 +0900

    add nextAuth

---
+++ app/login/form.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import React, { useState } from "react";
+
+export const Form = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    const res = await signIn("credentials", {
+      username,
+      password,
+      redirect: false,
+    });
+
+    if (res?.error) {
+      setError("Invalid username or password");
+    } else {
+      window.location.href = "/protect"; // ログイン成功後にリダイレクト
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
+    </>
+  );
+};
---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import { Form } from "./form";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
